8e682be6e35c7f8df495215d46d756c55bacc778
Jack Nerdie <nerdiejack@gmail.com>
2018-06-25 11:45:09 +0200

    Had Dive.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Dice {
-    
+
+    private int number;
+
+    public void roll(String go, int turns) {
+        if(go.equals("roll")) {
+            Random rand = new Random();
+            for (int i = 0; i < turns; i++) {
+                number = rand.nextInt(6) + 1;
+                System.out.print(getNumber() + " ");
+            }
+        } else
+            System.out.println("Invalid!");
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-import java.awt.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Type roll: ");
+        String rolling = in.next();
+        System.out.print("Turns: ");
+        int turns = in.nextInt();
+        Dice d = new Dice();
+        d.roll(rolling, turns);
 
     }
 }
